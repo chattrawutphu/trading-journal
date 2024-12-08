4e2d763d0916461602d2013434f508bd8fc55e95
Tar - Chattrawut <chattrawut.p@softdebut.com>
2024-12-08 23:43:43 +0700

    refactor: remove cron job for subscription expiration check and switch to Mongo Atlas trigger

--- src/lib/server/schedulers/subscriptionScheduler.js
+++ src/lib/server/schedulers/subscriptionScheduler.js
@@ -9,13 +9,15 @@ mongoose.set('bufferTimeoutMS', 30000); // 30 seconds
 // Get the cron schedule from environment variables or default to every hour
 const CRON_SCHEDULE = process.env.CRON_SCHEDULE || '0 * * * *';
 
-// Schedule the task to run based on the configured schedule
-cron.schedule(CRON_SCHEDULE, async () => {
-    try {
-        console.log('Running subscription expiration check...');
-        await Subscription.updateExpiredSubscriptions();
-        console.log('Subscription expiration check completed.');
-    } catch (error) {
-        console.error('Error updating expired subscriptions:', error);
-    }
-});
+// Remove the cron job
+// cron.schedule(CRON_SCHEDULE, async () => {
+//     try {
+//         console.log('Running subscription expiration check...');
+//         await Subscription.updateExpiredSubscriptions();
+//         console.log('Subscription expiration check completed.');
+//     } catch (error) {
+//         console.error('Error updating expired subscriptions:', error);
+//     }
+// });
+
+//เปลี่ยนไปใช้ mongo atlas trigger
